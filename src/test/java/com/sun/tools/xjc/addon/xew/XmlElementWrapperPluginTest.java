6d486733c0d41fe6e482004a0aa3f29d1ea8b6a7
Daniel Muhra <daniel.muhra@caperwhite.com>
2021-07-02 15:16:34 +0200

    Converted to JAXB 3.0

--- src/test/java/com/sun/tools/xjc/addon/xew/XmlElementWrapperPluginTest.java
+++ src/test/java/com/sun/tools/xjc/addon/xew/XmlElementWrapperPluginTest.java
@@ -40,10 +40,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
 import javax.xml.validation.SchemaFactory;
 
 import com.sun.tools.xjc.BadCommandLineException;
